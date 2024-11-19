365c557a21d0f7ce6a233248077eb6ee4b424af9
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2024-11-19 11:28:29 -0300

    productosempresa

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
 import logo from "/public/images/logo.png";
 
-const menuItems = ["Inicio", "Nosotros", "Servicios", "Catálogo", "Contacto"];
+const menuItems = ["Empresas", "Particulares"];
 
 
 export default function Nav() {
   return (
-      <nav className={` relative `}>
-        <ul className={` absolute top-10 left-1/2 -translate-x-1/2 w-min rounded-md bg-[linear-gradient(to_top,#1F1363,_#3A1E96,_#562BC8)] my-10 mx-auto px-10 shadow-lg shadow-[rgba(0,0,0,0.5)] flex flex-row gap-4 justify-between items-center`}>
-          <li
-          className={` w-max whitespace-nowrap flex flex-row items-center pr-10 `}>
-            <img src={logo.src} alt='' className={` inline-block w-20 h-auto `} />
-            <span className={` inline-block uppercase font-bold text-white text-xl `}>Vimerzion</span>
-          </li>
+    <nav className={` absolute top-10 left-1/2 -translate-x-1/2 w-min `}>
+      <ul className={` rounded-md bg-[linear-gradient(to_top,#1F1363,_#3A1E96,_#562BC8)] my-10 mx-auto px-10 shadow-lg shadow-[rgba(0,0,0,0.5)] flex flex-row gap-4 justify-between items-center`}>
+        <li
+          className={` w-max whitespace-nowrap flex flex-row items-center pr-10 py-2`}>
+          <img src={logo.src} alt='' className={` inline-block w-14 h-auto `} />
+          <span className={` inline-block uppercase font-bold text-white text-xl ml-3 `}>Vimerzion</span>
+        </li>
         {menuItems.map((item, index) => (
-      <li key={index}
-        className={` font-Inter cursor-pointer text-white text-opacity-80 hover:text-opacity-100 font-medium `} >
-        {item}
-      </li>
-    ))}
-        </ul>
-      </nav>
+          <li key={index}
+            className={` font-Inter cursor-pointer text-white text-opacity-80 hover:text-opacity-100 font-medium `} >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className={` relative `}>
 
         <div className={` max-w-4xl mx-auto `}>
-          <div className={`  text-center `}>
+          <div className={` `}>
             <img className={`  w-32 h-auto mx-auto mb-5 `} src={logo.src} alt='' />
             <p className={` font-Inter font-bold uppercase text-4xl text-white tracking-widest `}>Vimerzion</p>
             <p className={` font-Inter font-normal uppercase text-lg text-white tracking-widest `}>Realidad Virtual</p>
@@ -24,15 +24,32 @@ export default function Home() {
               Ya sea que estés explorando un videojuego, un metaverso o incluso participando en un entrenamiento virtual, la tecnología háptica te sumergirá por completo.
             </p>
             */}
-            <p className={` font-Inter font-light text-3xl text-white text-opacity-70 tracking-wider leading-relaxed mt-24 text-center `}>
-            En Vimerzion, lideramos el futuro de los eventos con experiencias de realidad virtual inmersivas. Ya sea para empresas o eventos particulares, transformamos tus ideas en soluciones innovadoras que cautivan y sorprenden
-</p>
+            <p className={` font-Inter font-light text-3xl text-white text-opacity-70 tracking-wider leading-relaxed mt-24 text-left hyphens-auto `}>
+              En Vimerzion, lideramos el futuro de los eventos con experiencias de realidad virtual inmersivas. Ya sea para empresas o eventos particulares, transformamos tus ideas en soluciones innovadoras que cautivan y sorprenden
+            </p>
 
-            
-<p className={` mt-24 font-Inter font-bold uppercase text-xl text-white tracking-widest `}>Eventos</p>
-            <p className={` mt-3 flex flex-row items-center justify-center font-Oswald font-medium text-3xl uppercase tracking-normal text-center p-0 m-0 `} style={{ filter: 'drop-shadow(0 0 0.6rem rgb(255,255,255,0.6))' }}>
-              <a href='' className={`  -mr-[0.81rem] px-10 py-3 inline-block text-black text-opacity-40 hover:text-opacity-80 bg-cyan-400 hover:bg-cyan-100 [clip-path:_polygon(0_0,calc(100%_-_1.62rem)_0,100%_100%,calc(0%_+_1.62rem)_100%)] cursor-pointer transition-all duration-300 ease-in-out `} >Empresas</a>
-              <a href='' className={`  -ml-[0.81rem] px-10 py-3 inline-block text-white text-opacity-50 hover:text-opacity-100 bg-fuchsia-500 bg-opacity-80 hover:bg-opacity-100 [clip-path:_polygon(0_0,calc(100%_-_1.62rem)_0,100%_100%,calc(0%_+_1.62rem)_100%)] cursor-pointer transition-all duration-300 ease-in-out `} >Particulares</a>
+            <div className={` mt-24 flex flex-col md:flex-row gap-8 `}>
+              <div>
+                <h3 className={` text-left font-Inter font-bold uppercase text-xl text-white tracking-widest `}>Servicios Empresas</h3>
+                <p className={` font-Inter font-light text-2xl text-white text-opacity-70 tracking-wider leading-relaxed mt-3 text-left hyphens-auto `}>
+                  Eventos corporativos, activaciones publicitarias, team building, etc. Experiencias VR destacadas.</p>
+              </div>
+              <div>
+                <h3 className={` text-left font-Inter font-bold uppercase text-xl text-white tracking-widest `}>Servicios Particulares</h3>
+                <p className={` font-Inter font-light text-2xl text-white text-opacity-70 tracking-wider leading-relaxed mt-3 text-left hyphens-auto `}>
+                  Fiestas de cumpleaños, matrimonios, graduaciones, bautizos, uso en casa, etc. Experiencias VR destacadas.</p>
+              </div>
+            </div>
+
+
+            <h2 className={` mt-24 font-Inter font-bold uppercase text-xl text-white tracking-widest `}>Cotiza aquí</h2>
+            <p className={` mt-3 flex flex-row items-center justify-center font-Oswald font-medium text-3xl uppercase tracking-normal text-center p-0 m-0 `} >
+              <span className={` transition-all duration-300 ease-in-out hover:drop-shadow-[0_0_0.6rem_rgba(255,255,255,255.25)] `} >
+                <a href='empresasproductos/' className={` -mr-[0.81rem] px-10 py-3 inline-block text-black text-opacity-40 hover:text-opacity-80 bg-cyan-400 hover:bg-cyan-100 [clip-path:_polygon(0_0,calc(100%_-_1.62rem)_0,100%_100%,calc(0%_+_1.62rem)_100%)] cursor-pointer transition-all duration-300 ease-in-out `} >Empresas</a>
+              </span>
+              <span className={` transition-all duration-300 ease-in-out hover:drop-shadow-[0_0_0.6rem_rgba(255,255,255,255.25)] `} >
+                <a href='' className={` -ml-[0.81rem] px-10 py-3 inline-block text-white text-opacity-50 hover:text-opacity-100 bg-fuchsia-600 hover:bg-fuchsia-500 [clip-path:_polygon(0_0,calc(100%_-_1.62rem)_0,100%_100%,calc(0%_+_1.62rem)_100%)] cursor-pointer transition-all duration-300 ease-in-out `} >Particulares</a>
+              </span>
             </p>
 
           </div>
